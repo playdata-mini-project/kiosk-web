9e75080b35f8930214a45ee26e7ec7785e70bc55
ktk8916 <ktk8916@naver.com>
2023-06-18 13:54:10 +0900

    전광판 기능 구상

---
+++ src/main/java/com/playdata/kiosk/utility/kitchen/Barista.java
@@ -0,0 +1,16 @@
+package com.playdata.kiosk.utility.kitchen;
+
+public class Barista {
+
+    public void 알바야커피만들어라(){
+        //대충 주문정보(유저, 상품, 주문수량...)받아옴
+        //커피머신으로 커피만듬
+        //대기열에 추가함
+    }
+    public void 알바야주문번호확인해라(){
+        //대기열에 있는 정보들
+        //이쁘게 만들어서 리턴해주기..
+        //혹은 이쁘게 만드는건 order queue에서 하고
+        // 단순 리턴 ?
+    }
+}
