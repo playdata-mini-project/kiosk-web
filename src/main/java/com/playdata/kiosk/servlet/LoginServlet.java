003c577344cc447de4dedc4f64edcccb5b0c5295
Ahnjaewon <vutus5@naver.com>
2023-06-20 16:44:27 +0900

    servlet update

--- src/main/java/com/playdata/kiosk/servlet/LoginServlet.java
+++ src/main/java/com/playdata/kiosk/servlet/LoginServlet.java
@@ -6,12 +6,14 @@ import com.playdata.kiosk.domain.User;
 import com.playdata.kiosk.dto.LoginDto;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
     @Override
